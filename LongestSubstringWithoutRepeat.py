b8d041bf345aec7ff715ea30f00f1869c2fbb691
Charlie Oh <mun5424@gmail.com>
2020-02-02 22:48:19 -0800

    Longest Substring without repeating

---
+++ LongestSubstringWithoutRepeat.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        map = {}
+        res = 0
+        i = 0
+        j = 0
+        while (i < len(s) and j < len(s)):
+            if s[j] in map:
+                i = max(map[s[j]], i)
+            res = max(res, j - i + 1)
+            map[s[j]] = j + 1
+            j += 1
+        return res
+    
