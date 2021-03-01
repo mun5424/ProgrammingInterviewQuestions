782b3423c61e6088d00582a6c16d4e4c49a21128
unknown <mun5424@gmail.com>
2021-03-01 00:36:44 -0800

    longest common subserquence

---
+++ subarraySum.py
@@ -0,0 +1,8 @@
+#Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k
+
+from typing import List
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
