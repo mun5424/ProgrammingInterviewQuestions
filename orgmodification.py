0db1ec7319ffc27f18c70dc49c3c8adcafaaf37d
unknown <mun5424@gmail.com>
2021-06-18 12:10:51 -0700

    org modification test

--- orgmodification.py
+++ orgmodification.py
@@ -26,5 +26,28 @@ def orgmodification(root):
     if not root.isengineer:
         return newreportees
     root.reportees = newreportees 
-    
+
     return [node]
+
+
+emp1 = Employee(1, True)
+emp2 = Employee(2, True)
+emp3 = Employee(3, False)
+emp4 = Employee(4, True)
+emp5 = Employee(5, False)
+emp6 = Employee(6, True)
+emp7 = Employee(7, True)
+
+emp1.reportees = [emp2, emp3, emp4]
+emp2.reportees = [emp5, emp6]
+emp3.reportees = [emp7]
+
+orgmodification(emp1)
+for e in emp1.reportees:
+    print(e.empid) 
+
+
+
+#             E1 
+#        E2    N3   E4
+#       E5 N6   E7 
